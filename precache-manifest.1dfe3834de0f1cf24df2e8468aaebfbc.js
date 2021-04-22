e50af7f04ae7b5f0a95ca89f43e7e7c2d97d03dc
Toni Filipovic <toni.filipovic18@gmail.com>
2021-04-22 22:00:00 +0200

    Updates

---
+++ precache-manifest.1dfe3834de0f1cf24df2e8468aaebfbc.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea77d231d00c4dd464a3309fa6de996",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc7a1721304d0244a51f",
+    "url": "/static/css/main.ba169df4.chunk.css"
+  },
+  {
+    "revision": "328af181ac04789ab71d",
+    "url": "/static/js/2.0f6adc50.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0f6adc50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc7a1721304d0244a51f",
+    "url": "/static/js/main.5bfdfe65.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "e241a06518c40f34d8b5056be0bc3e7d",
+    "url": "/static/media/AlgebraFED.e241a065.jpg"
+  },
+  {
+    "revision": "ff7778bcebaa2b6f4564d738ca3a723d",
+    "url": "/static/media/CSS – izgled i dizajn.ff7778bc.jpg"
+  },
+  {
+    "revision": "03dbad1e215ee518cec96b10106fc8b0",
+    "url": "/static/media/Experience.03dbad1e.jpg"
+  },
+  {
+    "revision": "6c1dc4a3c8d883829912e205fe280721",
+    "url": "/static/media/HTML – struktura i sadrzaj.6c1dc4a3.jpg"
+  },
+  {
+    "revision": "c9693b5a41484386f32a4e1d68705b24",
+    "url": "/static/media/HTML&CSS.c9693b5a.jpg"
+  },
+  {
+    "revision": "28626c7811e3f3284c1d34da97a44114",
+    "url": "/static/media/JavaScript – klijentsko skriptiranje.28626c78.jpg"
+  },
+  {
+    "revision": "40d3beb45ea530dce8247a387277bfc5",
+    "url": "/static/media/ProfilePhoto.40d3beb4.jpg"
+  },
+  {
+    "revision": "e7023cce46111a6625093f84477c2a93",
+    "url": "/static/media/Razvojna okolina.e7023cce.jpg"
+  },
+  {
+    "revision": "5407f75498a48d8b676845d6f861f914",
+    "url": "/static/media/React Developer Course.5407f754.jpg"
+  },
+  {
+    "revision": "bcdc20ae4454384efea70ccf5502a9d8",
+    "url": "/static/media/ReactJS – reaktivna arhitektura.bcdc20ae.jpg"
+  },
+  {
+    "revision": "20550f65db48f56f92b6eec836ecf842",
+    "url": "/static/media/Web Developer Bootcamp.20550f65.jpg"
+  },
+  {
+    "revision": "6562f38a2229cb8b3bcc98702e579dd2",
+    "url": "/static/media/Web korisnicka sucelja.6562f38a.jpg"
+  },
+  {
+    "revision": "74c479a34099c95719865645b24d7c6c",
+    "url": "/static/media/croatia-flag.74c479a3.png"
+  },
+  {
+    "revision": "982fc9186c87d552164af071bf37c838",
+    "url": "/static/media/js2018.982fc918.jpg"
+  },
+  {
+    "revision": "2aaa8b2209d698052a159a1b316e2949",
+    "url": "/static/media/uk-flag.2aaa8b22.png"
+  },
+  {
+    "revision": "4392b765c2f8e3f8961f409f39eca1f8",
+    "url": "/static/media/wordpress.4392b765.png"
+  }
+]);
